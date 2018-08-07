13044949a5f3f07be5b4742ffba66051f6ae41e3
timothyalfares <timothy4173@gmail.com>
2018-08-07 22:35:41 +1000

    Add skeleton elm project

---
+++ Todo.elm
@@ -0,0 +1,19 @@
+module Todo exposing (..)
+
+import Html exposing (div, text)
+
+main = Html.beginnerProgram { model = 0, view = view, update = update }
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+view model =
+  div []
+    [ text ("asdf")]
