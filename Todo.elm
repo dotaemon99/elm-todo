3402d5d2ee323ef369af2d7a19f9fb77ce08a257
timothyalfares <timothy4173@gmail.com>
2018-08-09 11:06:29 +1000

    Add todos header

--- Todo.elm
+++ Todo.elm
@@ -40,11 +40,42 @@ todosContainer =
       , backgroundColor (rgb 255 255 255)
       ]
     ]
-    [ div
-      [ css [] ]
-      [ h2
-        [ css [] ]
-        [ text "Wednesday, 8th" ]
-      ]
-    ]
+    [ todosHeader ]
     
+todosHeader : Html msg
+todosHeader =
+  div
+    [ css 
+      [ padding (Css.em 3)
+      , paddingTop (Css.em 1.5)
+      , paddingBottom (Css.em 1.5)
+      , backgroundColor (rgb 252 252 252)
+      , displayFlex
+      , flexDirection row
+      , border (px 0)
+      , borderBottom (px 0.5)
+      , borderColor (rgb 221 221 221)
+      , borderStyle solid
+      ] 
+    ]
+    [ h2
+      [ css 
+        [ fontSize (Css.em 1.8)
+        , color (rgb 255 87 51)
+        , textAlign left
+        , fontFamilies [ "Helvetica" ] 
+        ] 
+      ]
+      [ text "Wednesday, 8th" ],
+      h4
+      [ css 
+        [ fontSize (Css.em 1.2)
+        , color (rgb 175 175 175)
+        , textAlign right
+        , fontFamilies [ "Helvetica" ]
+        , alignSelf center
+        , flex (int 1)
+        ] 
+      ]
+      [ text "0 task" ]
+    ]
